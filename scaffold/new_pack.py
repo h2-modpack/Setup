19333b33e0db08083420e9781db17524d1a083a9
maybe-adamant <maybe.adamant@gmail.com>
2026-04-21 23:05:43 -0400

    Generate keep-a-changelog coordinator changelog

--- scaffold/new_pack.py
+++ scaffold/new_pack.py
@@ -130,7 +130,13 @@ hades-ii = [ "mods", ]
 CHANGELOG_MD = """\
 # Changelog
 
-## 1.0.0
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
+
+## [Unreleased]
+
+## [1.0.0]
 
 - Initial release
 """
