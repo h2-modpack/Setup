2289c2f21fc9a1a22cf5bc857b2026faf3f20286
maybe-adamant <maybe.adamant@gmail.com>
2026-05-03 01:27:16 -0400

    updating changelog and readme

--- scaffold/new_pack.py
+++ scaffold/new_pack.py
@@ -130,10 +130,6 @@ hades-ii = [ "mods", ]
 CHANGELOG_MD = """\
 # Changelog
 
-All notable changes to this project will be documented in this file.
-
-The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
-
 ## [Unreleased]
 
 ## [1.0.0]
